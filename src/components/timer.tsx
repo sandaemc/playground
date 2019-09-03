e0a621240e2cdec865b37a766a35388287d49492
Sandae Macalalag <sandaemc@gmail.com>
2019-09-03 22:29:37 +0800

    added timer

---
+++ src/components/timer.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import { useInterval } from 'react-use';
+
+export type TimerComponentProps = {
+  initialValue: number;
+  maxValue: number;
+  label: string;
+};
+
+export function TimerComponent({ initialValue, label, maxValue }: TimerComponentProps) {
+    const [currentValue, setCurrentValue] = useState(initialValue);
+
+    useInterval(() => {
+        console.log(currentValue);
+       setCurrentValue(currentValue + 1);
+    }, 1000);
+
+
+  return (
+    <div className={`c100 p${(currentValue / maxValue) * 100} blue`}>
+      <span>{label} {Math.ceil((currentValue / maxValue) * 100)}</span>
+      <div className="slice">
+        <div className="bar"/>
+        <div className="fill"/>
+      </div>
+    </div>
+  );
+}
--- src/pages/project-components/project-view.tsx
+++ src/pages/project-components/project-view.tsx
@@ -1,41 +1,33 @@
 import React from "react";
-import {createStyles, Theme, makeStyles} from "@material-ui/core/styles";
-import {Project} from "../../model-contracts/project";
-import PlayArrow from "@material-ui/icons/PlayArrow";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import { Project } from "../../model-contracts/project";
 import Container from "@material-ui/core/Container";
-import Fab from '@material-ui/core/Fab';
-import {Grid, Paper} from "@material-ui/core";
-import {ProgressIndicatorComponent} from "../../components/progress-indicator";
+import {TimerComponent} from "../../components/timer";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            width: "100%",
-            backgroundColor: theme.palette.background.paper,
-        }
-    })
+  createStyles({
+    root: {
+      width: "100%",
+      backgroundColor: theme.palette.background.paper
+    }
+  })
 );
 
 export type ProjectViewComponentProps = {
-    project: Project;
+  project: Project;
 };
 
-export function ProjectViewComponent({project}: ProjectViewComponentProps) {
-    const classes = useStyles();
+export function ProjectViewComponent({ project }: ProjectViewComponentProps) {
+  const classes = useStyles();
 
-    return <Container maxWidth="xs" className={classes.root}>
-            <h1>{project.name}</h1>
+  return (
+    <Container maxWidth="xs" className={classes.root}>
+      <h1>{project.name}</h1>
 
-                <div className="c100 p65 blue">
-                    <span onClick={() => alert("Hello")}>stop</span>
-                    <div className="slice">
-                        <div className="bar"></div>
-                        <div className="fill"></div>
-                    </div>
-                </div>
+        <TimerComponent initialValue={20} maxValue={120} label="Pause"/>
 
-        </Container>;
-
+    </Container>
+  );
 }
 
 /*
