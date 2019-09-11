32e3ac8c0a8c42d783efa8a289414947cef33bcc
Sandae Macalalag <sandaemc@gmail.com>
2019-09-11 09:29:59 +0800

    added web version

---
+++ app.py
@@ -0,0 +1,20 @@
+from chalice import Chalice
+from todo import add_task
+
+app = Chalice(app_name="recurrist")
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return {'app': 'Recurrist v1'}
+
+
+@app.route('/', methods=['POST'])
+def create_task():
+    request = app.current_request
+    body = request.json_body
+
+    add_task(body['name'], body['project'], body['labels'], None)
+
+    return {'done': 'ok'}
+
---
+++ todo.py
@@ -0,0 +1,35 @@
+import os
+from todoist.api import TodoistAPI
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api = TodoistAPI(os.getenv('TODOIST_TOKEN'))
+api.sync()
+
+
+def get_resource(key, source):
+    result = list(filter(lambda x: re.search(key, x['name']), api.state[source]))
+    if not result:
+        raise Exception(f"{key} in {source} not found")
+
+    return next(iter(result))
+
+
+def _get_project(key):
+    return get_resource(key, 'projects')
+
+
+def _get_label(key):
+    return get_resource(key, 'labels')
+
+
+def add_task(name, project, labels, due):
+    """Main entry point for the script."""
+    project = _get_project(project)['id']
+    labels = list(map(lambda x: _get_label(x)['id'], labels))
+
+    api.items.add(name, project_id=project, labels=labels)
+    api.commit()
+
