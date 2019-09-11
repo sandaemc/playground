3dc1f23d44f31103d6ee80b7b9b7606733fedf9c
Sandae Macalalag <sandaemc@gmail.com>
2019-09-12 06:12:36 +0800

    new tasks

--- cron/index.js
+++ cron/index.js
@@ -15,14 +15,25 @@ function addTask(name, project, labels, due) {
 
 // m h DoM M DoW
 cron.schedule('0 0 20 * *', () => { // every 20th of the month
-//cron.schedule('* * * * *', () => { // every 20th of the month
-    addTask('Pay Globe - 09176294439', 'Inbox', ['ThisWeek'], "26th");
-    addTask('Pay Globe At Home Wifi', 'Inbox', ['ThisWeek'], "26th");
-    addTask('Pay Credit Card', 'Inbox', ['ThisWeek'], "26th");
-    addTask('Transfer 13K to EastWest', 'Inbox', ['ThisWeek'], "26th");
+    // Finance
+    addTask('Pay Globe - 09176294439', 'Inbox', ['ThisWeek', 'Finance'], "26th");
+    addTask('Pay Globe At Home Wifi', 'Inbox', ['ThisWeek', 'Finance'], "26th");
+    addTask('Pay Credit Card', 'Inbox', ['ThisWeek', 'Finance'], "26th");
+    addTask('Transfer 13K to EastWest', 'Inbox', ['ThisWeek', 'Finance'], "26th");
+
+    // Personal
+    addTask('Schedule A Date', 'Inbox', ['ThisWeek', 'With_Cristina'], '25th');
 });
 
 cron.schedule('0 0 10 * *', () => { // every 10th of the month
-    addTask('Text St. Peter Agent', 'Inbox', ['ThisWeek', 'To_Tacloban'], "15th");
-    addTask('Pay St. Peter', 'Inbox', ['ThisMonth', 'At_Peerless'], "16th");
+    // Finance
+    addTask('Text St. Peter Agent', 'Inbox', ['ThisWeek', 'To_Tacloban', 'Finance'], "15th");
+    addTask('Pay St. Peter', 'Inbox', ['ThisMonth', 'At_Peerless', 'Finance'], "16th");
 });
+
+cron.schedule('0 0 */2 * *', () => { //every 2 days
+    // Home
+    addTask('Check Weather', 'Inbox', ['Morning'], 'today');
+    addTask('Take Trash', 'Inbox', ['To_VSU', 'Chore', 'Evening'], 'today');
+});
+
