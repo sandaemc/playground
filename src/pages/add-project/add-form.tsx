1981e30873d9910fb7b09b4588a3aa9e837caaf9
Sandae Macalalag <sandaemc@gmail.com>
2019-09-16 07:10:24 +0800

    fixed slider

--- src/pages/add-project/add-form.tsx
+++ src/pages/add-project/add-form.tsx
@@ -1,115 +1,132 @@
-import React, {ChangeEvent, useEffect} from "react";
-import {makeStyles, Theme} from "@material-ui/core/styles";
+import React, { ChangeEvent, useEffect } from "react";
+import { makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import Typography from '@material-ui/core/Typography';
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import {Project} from "../../models/project";
-import Slider from '@material-ui/core/Slider';
-
+import { Project } from "../../models/project";
+import Slider from "@material-ui/core/Slider";
 
 const useStyles = makeStyles((theme: Theme) => ({
-    container: {
-        display: "flex",
-        flexWrap: "wrap"
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-    scheduleContainer: {
-    }
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  sliderGrid: {
+    marginTop: theme.spacing(2),
+    height: theme.spacing(2)
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  },
+  scheduleContainer: {}
 }));
 
 type Props = {
-    onChange: any,
-    initial: Project
+  onChange: any;
+  initial: Project;
 };
 
 const labels = {
-    "MO": "Monday",
-    "TU": "Tuesday",
-    "WE": "Wednesday",
-    "TH": "Thursday",
-    "FR": "Friday"
+  MO: "Monday",
+  TU: "Tuesday",
+  WE: "Wednesday",
+  TH: "Thursday",
+  FR: "Friday"
 };
 
 export function AddFormComponent(props: Props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    function handleChange(event: React.ChangeEvent<{ name?: string; value: unknown }>) {
-        props.onChange({...props.initial, [event.target.name as string]: event.target.value});
-    }
+  function handleChange(
+    event: React.ChangeEvent<{ name?: string; value: unknown }>
+  ) {
+    props.onChange({
+      ...props.initial,
+      [event.target.name as string]: event.target.value
+    });
+  }
 
-    const handleSliderChange = (name: string) => (event: any, value: number | number[]) => {
-        const schedules = props.initial.schedules;
-        const index = schedules.findIndex(c => c.day === name);
-        const schedule = schedules[index];
+  const handleSliderChange = (name: string) => (
+    event: any,
+    value: number | number[]
+  ) => {
+    const schedules = props.initial.schedules;
+    const index = schedules.findIndex(c => c.day === name);
+    const schedule = schedules[index];
 
-        if (schedule.goal === (value as number))
-            return;
+    if (schedule.goal === (value as number)) return;
 
-        schedules.splice(index, 1, {...schedule, goal: value as number});
+    schedules.splice(index, 1, { ...schedule, goal: value as number });
 
-        props.onChange({
-            ...props.initial,
-            schedules
-        });
-    };
+    props.onChange({
+      ...props.initial,
+      schedules
+    });
+  };
 
-    return (
-        <Grid container>
-            <Grid item xs={12}>
-                {(new Date()).toISOString()}
-                <FormControl fullWidth>
-                    <TextField
-                        inputProps={{
-                            name: 'name'
-                        }}
-                        label="Name"
-                        value={props.initial.name}
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <FormControl fullWidth>
-                    <InputLabel htmlFor="color">Color</InputLabel>
-                    <Select
-                        value={props.initial.color}
-                        className={classes.selectEmpty}
-                        onChange={handleChange}
-                        inputProps={{
-                            name: 'color',
-                            id: 'color',
-                        }}>
-                        <MenuItem value="BLUE">Blue</MenuItem>
-                        <MenuItem value="ORANGE">Orange</MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        {new Date().toISOString()}
+        <FormControl fullWidth>
+          <TextField
+            inputProps={{
+              name: "name"
+            }}
+            label="Name"
+            value={props.initial.name}
+            margin="normal"
+            onChange={handleChange}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="color">Color</InputLabel>
+          <Select
+            value={props.initial.color}
+            className={classes.selectEmpty}
+            onChange={handleChange}
+            inputProps={{
+              name: "color",
+              id: "color"
+            }}
+          >
+            <MenuItem value="BLUE">Blue</MenuItem>
+            <MenuItem value="ORANGE">Orange</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
 
-
-            <Grid item xs={12} sm={6}>
-                    {props.initial.schedules.map((sched, i) =>
-                        <div key={i}>
-                            <Typography gutterBottom>
-                                {labels[sched.day]}
-                            </Typography>
-                            <Slider
-                                defaultValue={0}
-                                step={1}
-                                marks
-                                min={0}
-                                max={10}
-                                onChangeCommitted={handleSliderChange(sched.day)}
-                                valueLabelDisplay="auto"/></div>)}
-            </Grid>
-        </Grid>
-    );
+      <Grid item xs={12} className={classes.sliderGrid}>
+        <Typography variant="overline" gutterBottom>
+          Pomodoro goals per day
+        </Typography>
+        {props.initial.schedules.map((sched, i) => (
+          <div key={i}>
+            <Typography variant="overline" gutterBottom>
+              {labels[sched.day]}
+            </Typography>
+            <Slider
+              defaultValue={0}
+              step={1}
+              marks
+              min={0}
+              max={10}
+              onChangeCommitted={handleSliderChange(sched.day)}
+              valueLabelDisplay="auto"
+            />
+          </div>
+        ))}
+      </Grid>
+    </Grid>
+  );
 }
--- src/pages/add-project/index.tsx
+++ src/pages/add-project/index.tsx
@@ -1,68 +1,68 @@
-import React, {useState} from "react";
-import {AddFormComponent} from "./add-form";
-import {AddProjectBarComponent} from "./add-project-bar";
-import {RouteComponentProps} from "react-router";
-import {addProject, Project} from "../../models/project";
+import React, { useState } from "react";
+import { AddFormComponent } from "./add-form";
+import { AddProjectBarComponent } from "./add-project-bar";
+import { RouteComponentProps } from "react-router";
+import { addProject, Project } from "../../models/project";
 import Grid from "@material-ui/core/Grid";
-import {createStyles, makeStyles, Theme} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import v4 from "uuid/v4";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        fab: {
-            position: "absolute",
-            bottom: theme.spacing(2),
-            right: theme.spacing(2)
-        },
-        root: {
-            flexGrow: 1
-        },
-        paper: {
-            padding: theme.spacing(2),
-            textAlign: 'center',
-            color: theme.palette.text.secondary,
-        },
-    })
+  createStyles({
+    fab: {
+      position: "absolute",
+      bottom: theme.spacing(2),
+      right: theme.spacing(2)
+    },
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary
+    }
+  })
 );
 
-export default ({history}: RouteComponentProps) => {
-    const classes = useStyles();
+export default ({ history }: RouteComponentProps) => {
+  const classes = useStyles();
 
-    const [project, setProject] = useState<Project>({
-        id: v4(),
-        name: "",
-        color: "BLUE",
-        tasks: [],
-        schedules: [
-            {day: "MO", goal: 0},
-            {day: "TU", goal: 0},
-            {day: "WE", goal: 0},
-            {day: "TH", goal: 0},
-            {day: "FR", goal: 0}
-        ]
-    });
+  const [project, setProject] = useState<Project>({
+    id: v4(),
+    name: "",
+    color: "BLUE",
+    tasks: [],
+    schedules: [
+      { day: "MO", goal: 0 },
+      { day: "TU", goal: 0 },
+      { day: "WE", goal: 0 },
+      { day: "TH", goal: 0 },
+      { day: "FR", goal: 0 }
+    ]
+  });
 
-    const onChange = (data: any) => setProject(data);
+  const onChange = (data: any) => setProject(data);
 
-    const onDone = () => {
-        addProject(project);
-        history.push("/");
-    };
+  const onDone = () => {
+    addProject(project);
+    history.push("/");
+  };
 
-    return (
-        <div className={classes.root}>
-            <Grid container>
-                <Grid item xs={12}>
-                    <AddProjectBarComponent
-                        onBackClick={() => history.push("/")}
-                        onDoneClick={onDone}
-                    />
-                    <Container maxWidth="xl">
-                        <AddFormComponent initial={project} onChange={onChange}/>
-                    </Container>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12}>
+          <AddProjectBarComponent
+            onBackClick={() => history.push("/")}
+            onDoneClick={onDone}
+          />
+          <Container maxWidth="xl">
+            <AddFormComponent initial={project} onChange={onChange} />
+          </Container>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
