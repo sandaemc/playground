a12f257c91714f835ed76293d8429954d90b4148
Sandae Macalalag <sandaemc@gmail.com>
2019-10-04 07:59:30 +0800

    cleanup

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,66 +1,39 @@
-import {v4} from 'uuid';
-import {Project} from "./project";
-import {Config} from "./config";
+import { v4 } from "uuid";
+import { Project } from "./project";
+import { Config } from "./config";
 
-const lowdb = require('lowdb');
-const LocalStorage = require('lowdb/adapters/LocalStorage');
+const lowdb = require("lowdb");
+const LocalStorage = require("lowdb/adapters/LocalStorage");
 
-const adapter = new LocalStorage('db');
+const adapter = new LocalStorage("db");
 const db = lowdb(adapter);
 
 export type DatabaseSchema = {
-    configs: Config[],
-    logs: [],
-    projects: Project[]
-}
+  configs: Config[];
+  logs: [];
+  projects: Project[];
+};
 
 const defaults: DatabaseSchema = {
-    configs: [
-        {
-            name: 'tick.sound.volume',
-            value: 0.05,
-            type: 'float'
-        },
-        {
-            name: 'ding.sound.volume',
-            value: 0.10,
-            type: 'float'
-        },
-        {
-            name: 'flow.sound.volume',
-            value: 0.10,
-            type: 'float'
-        },
-    ],
-    logs: [],
-    projects: [
-        {
-            id: v4(),
-            name: 'Pomometer',
-            color: "BLUE",
-            tasks: [],
-            schedules: [
-                {day: 'MO', goal: 2},
-                {day: 'TU', goal: 2},
-                {day: 'WE', goal: 0},
-                {day: 'TH', goal: 2},
-                {day: 'FR', goal: 2},
-            ]
-        },
-        {
-            id: v4(),
-            name: 'Work',
-            color: 'ORANGE',
-            tasks: [],
-            schedules: [
-                {day: 'MO', goal: 3},
-                {day: 'TU', goal: 3},
-                {day: 'WE', goal: 3},
-                {day: 'TH', goal: 3},
-                {day: 'FR', goal: 3}
-            ]
-        }
-    ]
+  configs: [
+    {
+      name: "tick.sound.volume",
+      value: 0.05,
+      type: "float"
+    },
+    {
+      name: "ding.sound.volume",
+      value: 0.1,
+      type: "float"
+    },
+    {
+      name: "flow.sound.volume",
+      value: 0.1,
+      type: "float"
+    }
+  ],
+  logs: [],
+  projects: []
 };
 
 db.defaults(defaults).write();
