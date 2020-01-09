193c42b32c6fad423e9d9550e06c57f6d986c6c6
Sandae Macalalag <sandaemc@gmail.com>
2020-01-09 15:52:29 +0800

    added seller

---
+++ handlers/seller.ts
@@ -0,0 +1,36 @@
+import AWS from 'aws-sdk'
+import uuid from 'uuid'
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const TableName = process.env.DYNAMODB_TABLE || ''
+if (!TableName) {
+  throw new Error('Please provide table name')
+}
+
+export async function index(event: any) {
+  const result = await dynamoDb.scan({ TableName }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items)
+  }
+}
+
+export async function create(event: any) {
+  const params = {
+    TableName,
+    Item: {
+      pk: `seller_${uuid.v4()}`,
+      sk: 'data',
+      data: {
+        hello: 'world'
+      }
+    }
+  }
+
+  await dynamoDb.put(params).promise()
+  return {
+    statusCode: 200,
+    body: JSON.stringify(params.Item)
+  }
+}
