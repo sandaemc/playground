57ecb931055d14f0e3379dfb6219d0c83b0460fb
Sandae Macalalag <sandaemc@gmail.com>
2020-03-30 15:39:55 +0800

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to renamer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/renamer/core_test.clj
@@ -0,0 +1,7 @@
+(ns renamer.core-test
+  (:require [clojure.test :refer :all]
+            [renamer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
